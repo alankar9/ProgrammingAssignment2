cbb53a0aaca6fc799286ae09bb4333e96ee36806
alankar9 <alankar9@gmail.com>
2016-05-22 15:29:40 +0900

    Update MakeCacheMatrix.R

--- MakeCacheMatrix.R
+++ MakeCacheMatrix.R
@@ -55,20 +55,3 @@ cacheSolve <- function(x, ...) {
   x$setinverse(inv)
   inv
 }
-
-
-
-
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
